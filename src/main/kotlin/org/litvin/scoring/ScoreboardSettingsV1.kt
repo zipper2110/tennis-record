7291e805ba9c7bbfce903a20560027fbc62a53fd
Your Name <your.email@example.com>
2026-09-24 13:35:15 +0400

    default scoreboard title changed

--- src/main/kotlin/org/litvin/scoring/ScoreboardSettingsV1.kt
+++ src/main/kotlin/org/litvin/scoring/ScoreboardSettingsV1.kt
@@ -55,7 +55,7 @@ data class ScoreboardSettingsV1(
     )
 
     companion object {
-        const val DEFAULT_TITLE = "Tournament or club name"
+        const val DEFAULT_TITLE = "Your tournament or club"
         const val APP_CREDIT = "TennisRecord app"
         const val MAX_TITLE_LENGTH = 40
         const val MIN_SIZE_PERCENT = 50
